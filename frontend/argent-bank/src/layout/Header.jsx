24f2cda8f67b20c710462ed0ea258cdbe7e5b2b2
long bin <david.lb90@gmail.com>
2023-08-31 16:05:27 +0200

    login pas static ok

--- frontend/argent-bank/src/layout/Header.jsx
+++ frontend/argent-bank/src/layout/Header.jsx
@@ -4,14 +4,16 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
   return (
     <nav className="main-nav">
-      <a className="main-nav-logo" href="./index.html">
+      <NavLink to="/" className="main-nav-logo">
         <img
           className="main-nav-logo-image"
           src="./img/argentBankLogo.png"
           alt="Argent Bank Logo"
         />
-        <h1 className="sr-only">Argent Bank</h1>
-      </a>
+      </NavLink>
+
+      <h1 className="sr-only">Argent Bank</h1>
+
       <div>
         <NavLink to="/login" className={"main-nav-item"}>
           <i className="fa fa-user-circle"></i>
--- frontend/argent-bank/src/pages/Login.jsx
+++ frontend/argent-bank/src/pages/Login.jsx
@@ -1,7 +1,35 @@
 import React from "react";
+import Layout from "../layout/Layout";
 
 const Login = () => {
-  return <div>Login hello</div>;
+  const handleClick = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <Layout className={"main bg-dark"}>
+      <section className="sign-in-content">
+        <i className="fa fa-user-circle sign-in-icon"></i>
+        <h1>Sign In</h1>
+        <form>
+          <div className="input-wrapper">
+            <label for="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div class="input-wrapper">
+            <label for="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div class="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label for="remember-me">Remember me</label>
+          </div>
+          <button class="sign-in-button" onClick={handleClick}>
+            Sign In
+          </button>
+        </form>
+      </section>
+    </Layout>
+  );
 };
 
 export default Login;
