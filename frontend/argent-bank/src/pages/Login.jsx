812e083eff40e5cebb2c4815aa87aca716fd5423
long bin <david.lb90@gmail.com>
2023-09-01 11:54:44 +0200

    integration partiel de RTKQUERY

--- frontend/argent-bank/src/pages/Login.jsx
+++ frontend/argent-bank/src/pages/Login.jsx
@@ -1,9 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import Layout from "../layout/Layout";
+import { useLoginMutation } from "../API/Authentification/api";
+import Cookies from "js-cookie";
 
 const Login = () => {
-  const handleClick = (e) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loginMutation, { isLoading, isError }] = useLoginMutation();
+
+  const handleLogin = async (e) => {
     e.preventDefault();
+    const credentials = {
+      email: email,
+      password: password,
+    };
+    try {
+      const response = await loginMutation(credentials);
+      console.log(credentials);
+      console.log(response);
+      const token = response.data.body.token;
+      console.log(token);
+
+      Cookies.set("authToken", token, {
+        httpOnly: false,
+        secure: true,
+        sameSite: "strict",
+      });
+
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      console.error("Erreur lors de la connexion :", error);
+    }
   };
   return (
     <Layout className={"main bg-dark"}>
@@ -12,20 +40,35 @@ const Login = () => {
         <h1>Sign In</h1>
         <form>
           <div className="input-wrapper">
-            <label for="username">Username</label>
-            <input type="text" id="username" />
+            <label>Email</label>
+            <input
+              type="text"
+              id="username"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-          <div class="input-wrapper">
-            <label for="password">Password</label>
-            <input type="password" id="password" />
+          <div className="input-wrapper">
+            <label>Password</label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </div>
-          <div class="input-remember">
+          <div className="input-remember">
             <input type="checkbox" id="remember-me" />
-            <label for="remember-me">Remember me</label>
+            <label>Remember me</label>
           </div>
-          <button class="sign-in-button" onClick={handleClick}>
-            Sign In
+          <button className="sign-in-button" onClick={handleLogin}>
+            {isLoading ? "Loading" : "Sign-In"}
           </button>
+          {isError ? (
+            <p>Identifiants erronés T'ES QU'UNE MERDE</p>
+          ) : (
+            <p>Connection Réussi bravo le DAV!!!</p>
+          )}
         </form>
       </section>
     </Layout>
