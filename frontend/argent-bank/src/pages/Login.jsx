8b5d9d124fe07d472a52a65fdbcbc1e75a1d430f
long bin <david.lb90@gmail.com>
2023-09-10 22:01:55 +0200

    minor change css

--- frontend/argent-bank/src/pages/Login.jsx
+++ frontend/argent-bank/src/pages/Login.jsx
@@ -60,6 +60,7 @@ const Login = () => {
   return (
     <Layout className={"bg-dark main"}>
       <UseCookies />
+
       <section className="sign-in-content">
         <i className="fa fa-user-circle sign-in-icon"></i>
         <h1>Sign In</h1>
